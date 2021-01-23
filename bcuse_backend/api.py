2002efa5df39d02a0486165b2d08e9cd4367d5ec
Fixed <egorfilatov2002@yandex.ru>
2021-01-23 10:18:26 +0300

    new api version (CRUD)

--- bcuse_backend/api.py
+++ bcuse_backend/api.py
@@ -1,9 +1,7 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-import random
+from flask import request, jsonify
+from flask_api import FlaskAPI, status, exceptions
 
-app = Flask(__name__)
-api = Api(app)
+app = FlaskAPI(__name__)
 
 students = [
 	{
@@ -44,54 +42,143 @@ students = [
 	},
 ]
 
-class Student(Resource):
-	def get(self, login=None):
+@app.route("/students/<string:login>", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def students(login):
+	if request.method == 'GET':
+		#login must be specified
 		if login == None:
-			return "Specify login", 400
+			return jsonify("Specify login"), 400
 
 		#connect to sol
 		
+		#find and return student with uniq login
 		for student in students:
-			if(student["login"] == login):
-				return student, 200
+			if student["login"] == login:
+				return jsonify(student), 200
 
-		return "Student not found", 404
-
-	def post(self, login):
-		parser = reqparse.RequestParser()
-		parser.add_argument("surname")
-		parser.add_argument("name")
-		parser.add_argument("middle_name")
-		parser.add_argument("password")
-		parser.add_argument("universities")
-		parser.add_argument("specialities")
-		parser.add_argument("sum_score")
-		parser.add_argument("score")
-		parser.add_argument("subjects")
-		params = parser.parse_args()
+		return jsonify("Student not found"), 404
+	elif request.method == 'POST':
+		#POST request example
+		#{"surname": "Sad", "name": "Sad", "middle_name": "Sad", "login": "Sad", "password": "sad", "universities": "sad", "specialities": "sad", "sum_score": 270, "score": [100, 100, 70], "subjects": ["rus", "math", "inf"]}
+		surname = request.form.get('surname')
+		if surname is None:
+			return jsonify("Specify surname"), 400
 		
+		name = request.form.get('name')
+		if name is None:
+			return jsonify("Specify name"), 400
+
+		middle_name = request.form.get('middle_name')
+		if middle_name is None:
+			return jsonify("Specify middle_name"), 400
+
+		password = request.form.get('password')
+		if password is None:
+			return jsonify("Specify password"), 400
+
+		universities = request.form.get('universities')
+		if universities is None:
+			return jsonify("Specify universities"), 400
+
+		specialities = request.form.get('specialities')
+		if specialities is None:
+			return jsonify("Specify specialities"), 400
+
+		sum_score = request.form.get('sum_score')
+		if sum_score is None:
+			return jsonify("Specify sum_score"), 400
+
+		score = request.form.get('score')
+		if score is None:
+			return jsonify("Specify score"), 400
+
+		subjects = request.form.get('subjects')
+		if subjects is None:
+			return jsonify("Specify subjects"), 400
+		
+		#check this login in db
 		for student in students:
-			if(student["login"] == login):
-				return f"Student with login {login} already exists", 400
+			if student["login"] == login:
+				return jsonify(f"Student with login {login} already exists"), 400
 		
+		#create new student
 		new_student = {
-			"surname": params["surname"],
-			"name": params["name"],
-			"middle_name": params["middle_name"],
-			"login": params["login"],
-			"password": params["password"],
-			"universities": params["universities"],
-			"specialities": params["specialities"],
-			"sum_score": params["sum_score"],
-			"score": params["score"],
-			"subjects": params["subjects"]
+			"surname": surname,
+			"name": name,
+			"middle_name": middle_name,
+			"login": login,
+			"password": password,
+			"universities": universities,
+			"specialities": specialities,
+			"sum_score": sum_score,
+			"score": score,
+			"subjects": subjects
 		}
 
 		#connect to sol
 		
 		students.append(new_student)
-		return "Student created", 201
+		return jsonify("Student created"), 201
+	elif request.method == 'PUT':
+		surname = request.form.get('surname')
+		if surname is None:
+			surname = [student for student in students if student["login"] == login][0]["surname"]
+		
+		name = request.form.get('name')
+		if name is None:
+			name = [student for student in students if student["login"] == login][0]["name"]
 
-api.add_resource(Student, "/students", "/students/", "/students/<string:login>")
-if __name__ == '__main__':
+		middle_name = request.form.get('middle_name')
+		if middle_name is None:
+			middle_name = [student for student in students if student["login"] == login][0]["middle_name"]
+
+		password = request.form.get('password')
+		if password is None:
+			pass = [student for student in students if student["login"] == login][0]["password"]
+
+		universities = request.form.get('universities')
+		if universities is None:
+			universities = [student for student in students if student["login"] == login][0]["universities"]
+
+		specialities = request.form.get('specialities')
+		if specialities is None:
+			specialities = [student for student in students if student["login"] == login][0]["specialities"]
+
+		sum_score = request.form.get('sum_score')
+		if sum_score is None:
+			sum_score = [student for student in students if student["login"] == login][0]["sum_score"]
+
+		score = request.form.get('score')
+		if score is None:
+			score = [student for student in students if student["login"] == login][0]["score"]
+
+		subjects = request.form.get('subjects')
+		if subjects is None:
+			subjects = [student for student in students if student["login"] == login][0]["subjects"]
+
+		students = [student for student in students if student["login"] != login]
+
+		new_student = {
+			"surname": surname,
+			"name": name,
+			"middle_name": middle_name,
+			"login": login,
+			"password": password,
+			"universities": universities,
+			"specialities": specialities,
+			"sum_score": sum_score,
+			"score": score,
+			"subjects": subjects
+		}
+
+		#connect to sol
+
+		students.append(new_student)
+		return jsonify("Student created"), 201
+	elif request.method == 'DELETE':
+		#find and delete student by login
+		students = [student for student in students if student["login"] != login]
+		return jsonify(f"Student with login {login} is deleted"), 200
+
+if __name__ == "__main__":
     app.run(debug=True)
