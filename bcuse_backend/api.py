d1cf99bd9857934a25cfd1e82e8ebe67de244f31
Fixed <egorfilatov2002@yandex.ru>
2021-01-29 16:51:22 +0300

    ready to connect to sol

--- bcuse_backend/api.py
+++ bcuse_backend/api.py
@@ -3,74 +3,27 @@ from flask_api import FlaskAPI, status, exceptions
 
 app = FlaskAPI(__name__)
 
-students = [
-	{
-		"surname": "Asd",
-		"name": "Asd",
-		"middle_name": "Asd",
-		"login": "Asd",
-		"password": "asd",
-		"universities": "asd",
-		"specialities": "asd",
-		"sum_score": 270,
-		"score": [100, 100, 70],
-		"subjects": ['rus', 'math', 'inf']
-	},
-	{
-		"surname": "Qwe",
-		"name": "Qwe",
-		"middle_name": "Qwe",
-		"login": "Qwe",
-		"password": "qwe",
-		"universities": "qwe",
-		"specialities": "qwe",
-		"sum_score": 170,
-		"score": [60, 40, 70],
-		"subjects": ['rus', 'math', 'phis']
-	},
-	{
-		"surname": "Zxc",
-		"name": "Zxc",
-		"middle_name": "Zxc",
-		"login": "Zxc",
-		"password": "zxc",
-		"universities": "zxc",
-		"specialities": "zxc",
-		"sum_score": 300,
-		"score": [100, 100, 100],
-		"subjects": ['rus', 'math', 'hist']
-	},
-]
-
-@app.route("/students/<string:login>", methods=['GET', 'POST', 'PUT', 'DELETE'])
-def students(login):
+@app.route("/student/<string:university>", methods=['GET'])
+def student_get(university):
 	if request.method == 'GET':
-		#login must be specified
-		if login == None:
-			return jsonify("Specify login"), 400
+		#university must be specified
+		if university == None:
+			return jsonify("Specify university"), 400
 
-		#connect to sol
+		#реализовать коннект к sol, использовать функцию get_all_information_for_university
+		#передать ей university, вывод записать в ret
 		
-		#find and return student with uniq login
-		for student in students:
-			if student["login"] == login:
-				return jsonify(student), 200
+		return jsonify(ret)
 
-		return jsonify("Student not found"), 404
-	elif request.method == 'POST':
+@app.route("/student/", methods=['POST'])
+def student_post():
+	if request.method == 'POST':
 		#POST request example
-		#{"surname": "Sad", "name": "Sad", "middle_name": "Sad", "login": "Sad", "password": "sad", "universities": "sad", "specialities": "sad", "sum_score": 270, "score": [100, 100, 70], "subjects": ["rus", "math", "inf"]}
-		surname = request.form.get('surname')
-		if surname is None:
-			return jsonify("Specify surname"), 400
+		#{"universities": ["MGU", "MIPT"], "specialities": [["polit", "info"], ["info"]], "login": "Asd", "password": "Asd"}
 		
-		name = request.form.get('name')
-		if name is None:
-			return jsonify("Specify name"), 400
-
-		middle_name = request.form.get('middle_name')
-		if middle_name is None:
-			return jsonify("Specify middle_name"), 400
+		login = request.form.get('login')
+		if login is None:
+			return jsonify("Specify login"), 400
 
 		password = request.form.get('password')
 		if password is None:
@@ -83,102 +36,42 @@ def students(login):
 		specialities = request.form.get('specialities')
 		if specialities is None:
 			return jsonify("Specify specialities"), 400
-
-		sum_score = request.form.get('sum_score')
-		if sum_score is None:
-			return jsonify("Specify sum_score"), 400
-
-		score = request.form.get('score')
-		if score is None:
-			return jsonify("Specify score"), 400
-
-		subjects = request.form.get('subjects')
-		if subjects is None:
-			return jsonify("Specify subjects"), 400
 		
-		#check this login in db
-		for student in students:
-			if student["login"] == login:
-				return jsonify(f"Student with login {login} already exists"), 400
-		
-		#create new student
-		new_student = {
-			"surname": surname,
-			"name": name,
-			"middle_name": middle_name,
-			"login": login,
-			"password": password,
-			"universities": universities,
-			"specialities": specialities,
-			"sum_score": sum_score,
-			"score": score,
-			"subjects": subjects
-		}
+		#реализовать коннект к sol, использовать функцию set_data
+		#передать ей universities, specialities, login, password, вывод записать в ret
 
-		#connect to sol
+		return jsonify("Universities created"), 201
+
+@app.route("/university/<string:spec>", methods=['GET'])
+def university_get(spec):
+	if request.method == 'GET':
+		#spec must be specified
+		if spec == None:
+			return jsonify("Specify speciality (spec)"), 400
+
+		#реализовать коннект к sol, использовать функцию get_all_students_for_speciality
+		#передать ей spec, вывод записать в ret
 		
-		students.append(new_student)
+		return jsonify(ret), 200
+
+@app.route("/university/", methods=['POST'])
+def university_post():
+	if request.method == 'POST':
+		#POST request example
+		#{"address": "0x...", "spec": "polit"}
+		
+		address = request.form.get('address')
+		if address is None:
+			return jsonify("Specify address"), 400
+		
+		spec = request.form.get('spec')
+		if spec is None:
+			return jsonify("Specify speciality (spec)"), 400
+
+		#реализовать коннект к sol, использовать функцию add_student
+		#передать ей address и spec
+		
 		return jsonify("Student created"), 201
-	elif request.method == 'PUT':
-		surname = request.form.get('surname')
-		if surname is None:
-			surname = [student for student in students if student["login"] == login][0]["surname"]
-		
-		name = request.form.get('name')
-		if name is None:
-			name = [student for student in students if student["login"] == login][0]["name"]
-
-		middle_name = request.form.get('middle_name')
-		if middle_name is None:
-			middle_name = [student for student in students if student["login"] == login][0]["middle_name"]
-
-		password = request.form.get('password')
-		if password is None:
-			pass = [student for student in students if student["login"] == login][0]["password"]
-
-		universities = request.form.get('universities')
-		if universities is None:
-			universities = [student for student in students if student["login"] == login][0]["universities"]
-
-		specialities = request.form.get('specialities')
-		if specialities is None:
-			specialities = [student for student in students if student["login"] == login][0]["specialities"]
-
-		sum_score = request.form.get('sum_score')
-		if sum_score is None:
-			sum_score = [student for student in students if student["login"] == login][0]["sum_score"]
-
-		score = request.form.get('score')
-		if score is None:
-			score = [student for student in students if student["login"] == login][0]["score"]
-
-		subjects = request.form.get('subjects')
-		if subjects is None:
-			subjects = [student for student in students if student["login"] == login][0]["subjects"]
-
-		students = [student for student in students if student["login"] != login]
-
-		new_student = {
-			"surname": surname,
-			"name": name,
-			"middle_name": middle_name,
-			"login": login,
-			"password": password,
-			"universities": universities,
-			"specialities": specialities,
-			"sum_score": sum_score,
-			"score": score,
-			"subjects": subjects
-		}
-
-		#connect to sol
-
-		students.append(new_student)
-		return jsonify("Student created"), 201
-	elif request.method == 'DELETE':
-		#find and delete student by login
-		students = [student for student in students if student["login"] != login]
-		return jsonify(f"Student with login {login} is deleted"), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
